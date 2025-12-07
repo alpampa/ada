8920a06261d7761f1b57d97d4c43094ad21faee8
alpampa <84278150+alpampa@users.noreply.github.com>
2025-12-07 23:26:33 +0300

    Refactor adalar.js to simplify data fetching

--- api/adalar.js
+++ api/adalar.js
@@ -1,58 +1,22 @@
-import fetch from "node-fetch";
-import * as cheerio from "cheerio";
-
-const hatlar = {
-  "kabatas-adalar": "https://sehirhatlari.istanbul/tr/seferler/ic-hatlar/adalar-hatlari/kabatas-adalar-177",
-  "adalar-besiktas": "https://sehirhatlari.istanbul/tr/seferler/ic-hatlar/adalar-hatlari/adalar-besiktas-769",
-  "bostanci-adalar-ring": "https://sehirhatlari.istanbul/tr/seferler/ic-hatlar/adalar-hatlari/bostanci-adalar-ring-hatti-770",
-  "buyukada-sedef": "https://sehirhatlari.istanbul/tr/seferler/ic-hatlar/adalar-hatlari/buyukada-sedef-adasi-hatti-895",
-  "maltepe-adalar": "https://sehirhatlari.istanbul/tr/seferler/ic-hatlar/adalar-hatlari/maltepe-buyukada-heybeliada-burgazada-kinaliada-hatti-2020",
-  "tuzla-adalar": "https://sehirhatlari.istanbul/tr/seferler/ic-hatlar/adalar-hatlari/tuzla-pendik-buyukada-hatti-3373"
-};
-
-// HTML tablodan JSON çıkarma fonksiyonu
-async function tabloyuCek(url) {
-  const html = await fetch(url).then(r => r.text());
-  const $ = cheerio.load(html);
-
-  // Tablodaki tüm satırları oku
-  const seferler = [];
-  $("table tr").each((i, el) => {
-    const kolonlar = [];
-    $(el)
-      .find("td")
-      .each((j, td) => {
-        kolonlar.push($(td).text().trim());
-      });
-
-    // boş satırları at
-    if (kolonlar.length > 0) {
-      seferler.push(kolonlar);
-    }
-  });
-
-  return seferler;
-}
-
 export default async function handler(req, res) {
   try {
-    const sonuc = {};
+    const baseUrl = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : "http://localhost:3000";
 
-    // Her hattı paralel çek
-    await Promise.all(
-      Object.entries(hatlar).map(async ([isim, url]) => {
-        sonuc[isim] = await tabloyuCek(url);
-      })
-    );
+    const response = await fetch(`${baseUrl}/hatlar.json`);
+    const data = await response.json();
 
-    res.status(200).json({
+    return res.status(200).json({
       ok: true,
-      hatlar: sonuc,
+      toplamHat: Object.keys(data.hatlar).length,
+      hatlar: data.hatlar
     });
-  } catch (err) {
-    res.status(500).json({
+
+  } catch (e) {
+    return res.status(500).json({
       ok: false,
-      hata: err.toString(),
+      hata: e.message
     });
   }
 }
